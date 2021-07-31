3e9a326926996d20da4a490fa136509aa0942aaa
ludovic gauvin <lulu91340@hotmail.fr>
2021-07-31 23:05:08 +0200

    améliration compteur plus style

--- script/app.js
+++ script/app.js
@@ -1,20 +1,29 @@
 let nombre = " ";
+let helloCompteur = " ";
+let worldCompteur = " ";
+let helloWorldCompteur = " ";
 
 for (let i = 1; i <= 100; i++) {
 
-    nombre += i + "<br>"
+    nombre += i + "<br>";
 
     if (i % 3 == 0 && i % 5 == 0) {
-       nombre = nombre.replace(i,"hello world") 
+       nombre = nombre.replace(i,"hello world");
+       helloWorldCompteur ++;
     }
 
     else if (i % 3 == 0) {
-        nombre = nombre.replace(i, "hello")
+        nombre = nombre.replace(i, "hello");
+        helloCompteur ++;
     }
 
     else if (i % 5 == 0) {
-        nombre = nombre.replace(i, "world")
+        nombre = nombre.replace(i, "world");
+        worldCompteur ++;
     }
 
     document.getElementById("compteur").innerHTML = nombre;  
+    document.getElementById("helloWorld").innerHTML = "hello World = " + helloWorldCompteur;
+    document.getElementById("hello").innerHTML = "hello = " + helloCompteur;
+    document.getElementById("world").innerHTML = "world = " + worldCompteur;
 }
